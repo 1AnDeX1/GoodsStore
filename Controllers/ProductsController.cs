2327ff8f70e3fc10270efe8c43a279bffcb62ee8
1AnDeX1 <104911523+1AnDeX1@users.noreply.github.com>
2023-12-11 19:16:50 +0200

    added detail and index pages for products

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GoodsStore.Data;
+using GoodsStore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GoodsStore.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context) 
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Products product = _context.Products.FirstOrDefault(p => p.ProductID == id); 
+            return View(product);
         }
     }
 }
--- Models/Products.cs
+++ Models/Products.cs
@@ -10,6 +10,7 @@ namespace GoodsStore.Models
         public string? Description { get; set; }
         public int Price { get; set; }
         public int Quantity { get; set; }
+        public string? Image {  get; set; }
         public ICollection<OrderItems>? OrderItems { get; set; }
         public ICollection<DeliveryQueue>? DeliveryQueues { get; set; }
     }
